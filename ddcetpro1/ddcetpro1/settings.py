a0496c477869e66fe432bdf02d3a663667911f58
harshgamer17 <harshprajapati2172@gmail.com>
2026-03-23 11:07:32 +0530

    debug true

--- ddcetpro1/ddcetpro1/settings.py
+++ ddcetpro1/ddcetpro1/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ======================================================
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
